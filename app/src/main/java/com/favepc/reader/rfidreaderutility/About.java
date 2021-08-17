0dcc030ab31d75b596ce50d016789361a42e2016
yanaromero <yanadgr@gmail.com>
2021-08-17 14:40:11 +0800

    added close button to about page

--- app/src/main/java/com/favepc/reader/rfidreaderutility/About.java
+++ app/src/main/java/com/favepc/reader/rfidreaderutility/About.java
@@ -3,7 +3,9 @@ package com.favepc.reader.rfidreaderutility;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class About extends AppCompatActivity {
@@ -19,5 +21,12 @@ public class About extends AppCompatActivity {
         teamList = (ListView)findViewById(R.id.aboutTeam);
         ArrayAdapter<String> teamMembers = new ArrayAdapter<String>(this, R.layout.activity_listview,R.id.textView,memberList );
         teamList.setAdapter(teamMembers);
+        Button closeBtn = findViewById(R.id.close_about);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
